d654d1bea787d5356a13ffd78346ccaff5821715
naufaldi <naufaldi.rafif@gmail.com>
2017-10-06 09:24:23 +0700

    3 finish

--- algoritm_basic/truncate.js
+++ algoritm_basic/truncate.js
@@ -1,8 +1,11 @@
-function truncate(str, num) {
-    var trun = '';
-    if (str.length > num) {
-        trun = str.slice(0, num - 3) + ' . . .';
-        return trun;
+function truncateString(str, num) {
+    // Clear out that junk in your trunk
+    if (str.length > num && num > 3) {
+        return str.slice(0, (num - 3)) + '...';
+    } else if (str.length > num && num <= 3) {
+        return str.slice(0, num) + '...';
+    } else {
+        return str;
     }
-    return str;
+
 }
